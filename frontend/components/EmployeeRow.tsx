645788dd966e2f98903034b680c1bba4052d09f6
Sohan Chowdhury <sohanchy.cs@gmail.com>
2023-05-09 16:03:39 -0700

    Visually working edit,cancel button -> changes rows to input boxes and back

--- frontend/components/EmployeeRow.tsx
+++ frontend/components/EmployeeRow.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Router from 'next/navigation'
 
 export type EmployeeProps = {
@@ -8,24 +8,43 @@ export type EmployeeProps = {
   salary: number;
 }
 
-const EmployeeRow: React.FC<{
-    employee: EmployeeProps, editMode: boolean }> = ({ employee, editMode = false }) => {
-    if(editMode){
-        <p>tbd</p>
+const EmployeeRow: React.FC<{employee: EmployeeProps}> = ({ employee}) => {
+
+    const [isEditMode, setEditMode] = useState(false);
+    const onEdit = () => {
+        setEditMode(true);
+    }
+    const onCancel = () => {
+        setEditMode(false);
+    }
+
+    if(isEditMode){
+        return (
+            <tr key={employee.id} className="align-middle">
+                <td style={{maxWidth: '2rem'}}><input className="form-control" value={employee.firstName}/></td>
+                <td style={{maxWidth: '2rem'}}><input className="form-control" value={employee.lastName}/></td>
+                <td style={{maxWidth: '2rem'}}><input className="form-control" value={employee.salary}/></td>
+                <td style={{minWidth: '4.5rem'}}>
+                <button type="button" className="btn btn-outline-success" onClick={onEdit}>Save</button>&nbsp;
+                <button type="button" className="btn btn-outline-danger" onClick={onCancel}>Cancel</button>
+                </td>          
+            </tr>   
+          )
     }
     else return (
         <tr key={employee.id} className="align-middle">
-          <td>{employee.firstName}</td>
-          <td>{employee.lastName}</td>
-          <td>${employee.salary}</td>
-          <td>
-            <button type="button" className="btn btn-outline-info">Edit</button>&nbsp;
+          <td style={{maxWidth: '2rem'}}>{employee.firstName}</td>
+          <td style={{maxWidth: '2rem'}}>{employee.lastName}</td>
+          <td style={{maxWidth: '2rem'}}>${employee.salary}</td>
+          <td style={{minWidth: '4.5rem'}}>
+            <button type="button" className="btn btn-outline-info" onClick={onEdit}>Edit</button>&nbsp;
             <button type="button" className="btn btn-outline-danger">Delete</button>
           </td>          
         </tr>   
   )
+
 }
 
 
 
-export default EmployeeRow
+export default EmployeeRow;
