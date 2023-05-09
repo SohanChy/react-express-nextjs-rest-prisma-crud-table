852565b625425b2e5bab77c0f7e905ea8ad5a7ad
Sohan Chowdhury <sohanchy.cs@gmail.com>
2023-05-09 13:06:48 -0700

    Initial config for frontend with next-react-jest

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+type Props = {
+  
+}
+
+const Home : React.FC<Props> = props => {
+  return (
+    <html>
+      <body>
+        <h1>Hello World</h1>
+      </body>
+    </html>
+  )
+}
+
+export default Home
