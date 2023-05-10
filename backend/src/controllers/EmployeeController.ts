466d64b1d98b866d03d51f444788611f27c548c3
Sohan Chowdhury <sohanchy.cs@gmail.com>
2023-05-09 22:34:17 -0700

    Add middleware for gracefully handling errors Add custom error for validation errors Add pagination support and validation to employeelist

--- backend/src/controllers/EmployeeController.ts
+++ backend/src/controllers/EmployeeController.ts
@@ -1,24 +1,50 @@
 import { PrismaClient } from '@prisma/client';
 import { log } from 'console';
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
+import { ValidationError } from '../middlewares/ApiErrors';
 
 export class EmployeeController {
 
     public static prismaClient = new PrismaClient()
 
-    public static async index(req: Request, res: Response): Promise<void> {
-        try {
-            const employees = await EmployeeController.prismaClient.employee.findMany({})
-            
-        res.status(200).json({ success: true, employees });
-        } 
-        catch (error: unknown) {
-            if (error instanceof Error) {
-                res.status(500).json({ success: false, message: error.message });
-            } else {
-                console.error(error);
-                res.status(500).json({ success: false, message: "Unknown error ocurred" });
-            }
+    public static async index(req: Request, res: Response, next: NextFunction): Promise<void> {
+
+        // 1 indexed
+        let pageNum = 1;
+        let limit = 10;
+        
+
+        // Validation - Should use package such as joi
+        if(req.query.pageNum){
+            pageNum = parseInt(req.query.pageNum as string, 10);
+        }
+
+        if(req.query.limit){
+            limit = parseInt(req.query.limit as string, 10);
+        }
+        
+        if (!Number.isInteger(Number(pageNum)) || !Number.isInteger(Number(limit))) {
+            next(new ValidationError("pageNum & limit must be integer"));
+        }
+
+        if (pageNum < 1 || limit < 1) {
+            next(new ValidationError("pageNum & limit must be greater than 1"));
+        }
+
+
+        // All errors handled by middleware
+        try{
+            const employees = await EmployeeController.prismaClient.employee.findMany({
+                skip: (pageNum - 1) * limit,
+                take: limit,
+            })    
+
+            res.status(200).json({ success: true, employees });
+        }
+        catch (err: unknown){
+            next(err);
+        }
+
     }
 
 }
